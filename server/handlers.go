d19411f2b90756a3b378968202cc83d6c1bbb6c0
scott <rinzencedev@gmail.com>
2025-01-24 16:57:09 +0000

    updated POST handler for MongoDB. Disabled PUT/ DELETE temporarily

--- server/handlers.go
+++ server/handlers.go
@@ -7,24 +7,16 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"github.com/segmentio/ksuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var validate = validator.New()
 
-// Tasks slice to seed task data - including dummy data
-var tasks = []Task{
-	{ID: ksuid.New(), Title: "task 1"},
-	{ID: ksuid.New(), Title: "task 2", Status: true},
-}
-
 // getTasks - responds with the list of all tasks as JSON.
 func getTasks(ctx *gin.Context) {
-	cursor, err := mongoClient.Database("task_manager").Collection("tasks").Find(context.TODO(), bson.D{{}}, options.Find().SetLimit(2))
+	cursor, err := mongoClient.Database("task_manager").Collection("tasks").Find(context.TODO(), bson.D{{}})
 	if err != nil {
 		respondWithError(ctx, http.StatusInternalServerError, "error fetching tasks", err.Error())
 		return
@@ -66,79 +58,83 @@ func getTaskByID(ctx *gin.Context) {
 }
 
 // postTask adds a task from JSON received in the request body.
-func postTask(context *gin.Context) {
+func postTask(c *gin.Context) {
 	var newTask Task
-	newTask.ID = ksuid.New()
+	newTask.ID = primitive.NewObjectID()
 	newTask.Created = time.Now().UTC()
 	newTask.Status = false
 
-	if err := context.BindJSON(&newTask); err != nil {
-		respondWithError(context, http.StatusBadRequest, "invalid JSON", err.Error())
+	if err := c.BindJSON(&newTask); err != nil {
+		respondWithError(c, http.StatusBadRequest, "Invalid JSON", err.Error())
 		return
-	}
-
-	if newTask.Status {
-		newTask.Status = false
 	}
 
 	if err := validate.Struct(newTask); err != nil {
-		respondWithError(context, http.StatusBadRequest, "validation error", err.Error())
+		respondWithError(c, http.StatusBadRequest, "Validation error", err.Error())
 		return
 	}
 
-	tasks = append(tasks, newTask)
-	context.IndentedJSON(http.StatusCreated, newTask)
+	_, err := mongoClient.Database("task_manager").Collection("tasks").InsertOne(context.TODO(), newTask)
+	if err != nil {
+		respondWithError(c, http.StatusInternalServerError, "Error inserting task", err.Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Task created successfully",
+		"data":    newTask,
+	})
 }
 
 // updateTask updates the task with an ID value that matches the id parameter sent by the client
 func updateTask(context *gin.Context) {
-	id := context.Param("id")
+	// id := context.Param("id")
 
-	var updatedFields struct {
-		Title  *string `json:"title" validate:"omitempty,min=1"`
-		Status *bool   `json:"status"`
-	}
+	// var updatedFields struct {
+	// 	Title  *string `json:"title" validate:"omitempty,min=1"`
+	// 	Status *bool   `json:"status"`
+	// }
 
-	if err := context.BindJSON(&updatedFields); err != nil {
-		respondWithError(context, http.StatusBadRequest, "invalid JSON", err.Error())
-		return
-	}
+	// if err := context.BindJSON(&updatedFields); err != nil {
+	// 	respondWithError(context, http.StatusBadRequest, "invalid JSON", err.Error())
+	// 	return
+	// }
 
-	if err := validate.Struct(updatedFields); err != nil {
-		respondWithError(context, http.StatusBadRequest, "validation error", err.Error())
-		return
-	}
+	// if err := validate.Struct(updatedFields); err != nil {
+	// 	respondWithError(context, http.StatusBadRequest, "validation error", err.Error())
+	// 	return
+	// }
 
-	for i, task := range tasks {
-		if task.ID.String() == id {
+	// for i, task := range tasks {
+	// 	if task.ID.String() == id {
 
-			if updatedFields.Title != nil {
-				tasks[i].Title = *updatedFields.Title
-			}
+	// 		if updatedFields.Title != nil {
+	// 			tasks[i].Title = *updatedFields.Title
+	// 		}
 
-			if updatedFields.Status != nil {
-				tasks[i].Status = *updatedFields.Status
-			}
+	// 		if updatedFields.Status != nil {
+	// 			tasks[i].Status = *updatedFields.Status
+	// 		}
 
-			context.IndentedJSON(http.StatusOK, tasks[i])
-			return
-		}
-	}
+	// 		context.IndentedJSON(http.StatusOK, tasks[i])
+	// 		return
+	// 	}
+	// }
 
-	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "task not found"})
+	// context.IndentedJSON(http.StatusNotFound, gin.H{"message": "task not found"})
 }
 
 // deleteTask deletes the task with an ID value matches the id parameter sent by the client
 func deleteTask(context *gin.Context) {
-	id := context.Param("id")
+	// id := context.Param("id")
 
-	for i, task := range tasks {
-		if task.ID.String() == id {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			context.IndentedJSON(http.StatusOK, gin.H{"message": "task deleted"})
-			return
-		}
-	}
+	// for i, task := range tasks {
+	// 	if task.ID.String() == id {
+	// 		tasks = append(tasks[:i], tasks[i+1:]...)
+	// 		context.IndentedJSON(http.StatusOK, gin.H{"message": "task deleted"})
+	// 		return
+	// 	}
+	// }
 
 	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "task not found"})
 }
--- server/models.go
+++ server/models.go
@@ -3,13 +3,13 @@ package main
 import (
 	"time"
 
-	"github.com/segmentio/ksuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Task represents data about a task
 type Task struct {
-	ID      ksuid.KSUID `json:"id" validate:"required"`
-	Title   string      `json:"title" validate:"required,min=1,max=140"`
-	Status  bool        `json:"status"`
-	Created time.Time   `json:"created" validate:"required"`
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	Title   string             `json:"title" validate:"required,min=1,max=140"`
+	Status  bool               `json:"status"`
+	Created time.Time          `json:"created" validate:"required"`
 }
