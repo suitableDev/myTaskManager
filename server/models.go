fb1ae0a707165b7d8d584272606dc5018e2a6ab6
scott <rinzencedev@gmail.com>
2025-02-16 13:45:07 +0000

    added user & updated to the task struct & handlers

--- server/models.go
+++ server/models.go
@@ -9,9 +9,11 @@ import (
 // Task represents data about a task
 type Task struct {
 	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	User    string             `json:"User" validate:"required"`
 	Title   string             `json:"title" validate:"required,min=1,max=140"`
 	Status  bool               `json:"status"`
 	Created time.Time          `json:"created" validate:"required"`
+	Updated time.Time          `json:"updated"`
 }
 
 type ErrorResponse struct {
