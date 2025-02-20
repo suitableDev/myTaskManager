2edd6196cd531c3279ed6fd49767538bd06d049a
scott <rinzencedev@gmail.com>
2025-02-20 14:25:12 +0000

    Revert "added userId and username of logged in user to PostTask function"
    This reverts commit 5c3a452f23b7f96598b09fab57d65af37a2cebb6.

--- server/models/taskModel.go
+++ server/models/taskModel.go
@@ -9,7 +9,6 @@ import (
 type Task struct {
 	ID      primitive.ObjectID `bson:"_id,omitempty"`
 	User    string             `json:"User" validate:"required"`
-	UserId  string             `json:"userId"`
 	Title   string             `json:"title" validate:"required,min=1,max=140"`
 	Status  bool               `json:"status"`
 	Created time.Time          `json:"created" validate:"required"`
